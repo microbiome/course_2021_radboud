0608973de1b5aaf7da96dd9d3cae1660119d771b
tvborm <tvborm@utu.fi>
2021-05-09 09:58:23 +0300

    Abundance plots are now relative

--- main.R
+++ main.R
@@ -6,6 +6,10 @@
 # This creates the html files at
 # https://microbiome.github.io/course_2021_radboud/
 
+# Sets the theme
+theme_set(theme_bw(20)) 
+
+# Renders notebooks
 rmarkdown::render("import.Rmd")
 rmarkdown::render("explore.Rmd")
 rmarkdown::render("alpha.Rmd")
