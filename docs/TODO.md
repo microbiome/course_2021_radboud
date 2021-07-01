20bc30187186b067a05216499a1eb8a5c80527ae
tvborm <tvborm@utu.fi>
2021-07-01 12:38:15 +0300

    gitbook-->bookdown

--- docs/TODO.md
+++
@@ -1,3 +0,0 @@
-## TODO
-
-- Example about altExp?
--- docs/main.R
+++ docs/main.R
@@ -1,30 +1,14 @@
 
+# This code below builds the Rmd file into a book
+authors <- "Leo Lahti, Tuomas Borman, Henrik Eckerman"
+library(bookdown)
+render_book(input = "index.Rmd", output_format = "bookdown::gitbook", output_dir = "docs")
 
-# This creates the md files
-# knitr::knit("install.Rmd")
+# Instead of render_book, you can run serve_book. The difference is that by serve_book
+# you can preview the book on your web browser live. However, when using serve_book you
+# should have servr package installed. 
 
-# This creates the md files at:
-# https://microbiome.github.io/course_2021_radboud/
-
-# The source files can be cloned from
-# https://github.io/microbiome.github.io/course_2021_radboud/
-
-# md files can be converted to html files and to GitBook with GitBook documentation tool
-# https://github.com/GitbookIO/gitbook
-
-# Terminal command 'gitbook build ./ docs' creates html files from md files
-# that are located in root folder. It stores html files to 'docs' folder. 
-# SUMMARY.md includes a list of files that will be rendered.
-
-# Sets the theme
-library(ggplot2)
-theme_set(theme_bw(20)) 
-
-# Renders notebooks
-rmarkdown::render("install.Rmd")
-rmarkdown::render("import.Rmd")
-rmarkdown::render("explore.Rmd")
-rmarkdown::render("alpha.Rmd")
-rmarkdown::render("beta.Rmd")
-rmarkdown::render("dmm.Rmd")
-rmarkdown::render("abundance.Rmd")
+# Installs servr package. Needs only to do once.
+# install.packages("servr")
+# starts local web browser and serve the HTML output
+# serve_book()
