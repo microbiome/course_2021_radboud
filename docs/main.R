a0b2aecf2b7f4c9be83a2306aa2d5189a940fc58
tvborm <tvborm@utu.fi>
2021-07-08 11:09:36 +0300

    Removed unnecessary information from main.R

--- docs/main.R
+++ docs/main.R
@@ -8,11 +8,5 @@ tse <- readRDS(file = "data/tse.rds")
 library(bookdown)
 render_book()
 
-# Instead of render_book, you can run serve_book. The difference is that by serve_book
-# you can preview the book on your web browser live. However, when using serve_book you
-# should have servr package installed. 
-
-# Installs servr package. Needs only to do once.
-# install.packages("servr")
-# starts local web browser and serve the HTML output
-# serve_book()
+# After you have rendered the book locally, you can observe rendered html files
+# that are located in docs subfolder.
